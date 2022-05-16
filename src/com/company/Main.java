c7a7ff485c5c9bf1e9dcc061a72879a3af5ef31e
Petar <99811846+petarxiv@users.noreply.github.com>
2022-05-16 00:26:51 -0400

    the updated commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //teamlead project
+        System.out.println("I am Groot!!!");
+
+        System.out.println("I am Groot2!!!");
+        System.out.println("I am Groot3!!!");
+
+        System.out.println("i am correct code");
+        System.out.println("i am not confused");
+        System.out.println("heloo my friend");
+    }
+}
